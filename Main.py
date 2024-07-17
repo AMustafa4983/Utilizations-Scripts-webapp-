7e4c667aeb39be93e5bfc0eaaf06be70d3ca515e
Abdelrahman Mohamed <abdelrahman.mohamed@fidelityunited.ae>
2024-07-17 13:45:21 +0400

    second commit

--- Main.py
+++ Main.py
@@ -4,3 +4,22 @@ st.title("Main Page")
 st.write('''
 This is Main Page.
 ''')
+
+ASOAP = st.Page(
+    "Pages/ASOAP.py", title="ASOAP", icon=":material/dashboard:", default=True
+)
+
+Beneficiary = st.Page(
+    "reports/Beneficiary.py", title="Beneficiary", icon=":material/dashboard:", default=True
+)
+
+Tools = st.Page(
+    "reports/Tools.py", title="Tools", icon=":material/dashboard:", default=True
+)
+
+pg = st.navigation(
+    {
+        "Files": [Tools],
+        "Data": [ASOAP, Beneficiary],
+    }
+)
